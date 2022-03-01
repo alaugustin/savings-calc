7c13658ef6a763f563c600ccbe16b878c5ebf9ee
Al Augustin <al@alaugustin.com>
2022-03-01 18:23:23 -0500

    input component

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,9 @@
 // import logo from './logo.svg';
-import React;
 import './App.css';
+import Input from './Input';
 import Label from './Label';
 
 function App() {
-  const [formData, updateFormData] = React.useState(initialFormData);
-
-  const handleChange = (e) => {
-    updateFormData({
-      ...formData,
-
-      // Trimming any whitespace
-      [e.target.name]: e.target.value.trim()
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(formData);
-    // ... submit to API or something
-  };
-
   return (
     <div>
       <h1>Main Page</h1>
@@ -29,21 +12,22 @@ function App() {
 
       <form id="mainForm" method="post">
         <p>Select a province</p>
-        <input type="radio" id="on" name="location" value="on" onChange={handleChange} />
+
+        <Input labelType={"radio"} labelId={"on"} labelName={"location"} labelValue={"on"} />
         <Label htmlFor="on" labelCopy="Ontario" /><br />
 
-        <input type="radio" id="mb" name="location" value="mb" onChange={handleChange} />
+        <Input labelType={"radio"} labelId={"mb"} labelName={"location"} labelValue={"mb"} />
         <Label htmlFor="mb" labelCopy="Manitoba" /><br />
 
         <Label htmlFor="gas" labelCopy="How many tanks of gas per week?" /><br />
-        <input type="number" id="gas" name="gas" onChange={handleChange} /><br />
+        <Input labelType={"number"} labelId={"gas"} labelName={"gas"} labelValue={""} /><br />
 
         <Label htmlFor="wash" labelCopy="How many car washes per week do you purchase along with gas?" /><br />
-        <input type="number" id="wash" name="wash" onChange={handleChange} /><br />
+        <Input labelType={"number"} labelId={"wash"} labelName={"wash"} labelValue={""} /><br />
 
         <div id="washTypeHolder">
           <Label htmlFor="washType" labelCopy="What type of car wash do you purchase?" /><br />
-          <select name="washType" id="washType" onChange={handleChange}>
+          <select name="washType" id="washType">
             <option value="">-----</option>
             <option value="basic">Basic</option>
             <option value="deluxe">Deluxe</option>
@@ -52,7 +36,7 @@ function App() {
         </div>
 
         <Label htmlFor="sundries" labelCopy="How much do you spend in-store per week?" /><br />
-        <input type="number" id="sundries" name="sundries" onChange={handleChange}/><br />
+        <Input labelType={"number"} labelId={"sundries"} labelName={"sundries"} labelValue={""} /><br />
 
         <button type="submit">Submit</button>
       </form>
