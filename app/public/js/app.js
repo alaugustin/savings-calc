69585b8e9d0d6e333bb466e741c8f6c7af7d75ae
aaug <aaug@caasco.ca>
2022-03-03 10:31:39 -0500

    namespace template

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,3 +1,101 @@
+let savingsCalculator = {
+  version: '1.0',
+  author: '',
+  project: 'Savings Calculator',
+  Date: '2022',
+
+  // -------------------- INITIALIZATION --------------------
+  init: function () {
+    let context = this;
+
+    // GLOBAL VARIABLES --------------------
+    context.config = {
+      // let
+      resultsDiv: document.getElementById("results"),
+      fuelDiscount: 0.03,
+      washDiscount: 0.10,
+      sundriesDiscount: 0.10,
+      fullTankLitres: 55,
+      weeksPerYear: 52,
+      // exchangeRate: exchangeRate,
+      // exchangeRateDate: ratesAsOf,
+      // userLocation: userData.location,
+      // numberOfTanks: userData.gas,
+      // numberOfWash: userData.wash,
+      // washType: userData.washType,
+      // sundries: userData.sundries,
+      // weeksPerYear: weeksPerYear,
+      // fullTankLitres: fullTankLitres,
+      // fuelDiscount: fuelDiscount,
+      // washDiscount: washDiscount,
+      // sundriesDiscount: sundriesDiscount,
+      location: {
+        on: {
+          taxRate: ontaxDataRate,
+          taxType: ontaxDataType,
+          GasPriceCad: onGasPriceCad,
+          carWash: {
+            basic: {
+              price: 9.99,
+            },
+            deluxe: {
+              price: 11.99,
+            },
+            ultimate: {
+              price: 13.99,
+            },
+          },
+        },
+        mb: {
+          taxRate: mbTaxDataRate,
+          taxType: mbTaxDataType,
+          GasPriceCad: mbGasPriceCad,
+          carWash: {
+            basic: {
+              price: 7.99,
+            },
+            deluxe: {
+              price: 9.99,
+            },
+            ultimate: {
+              price: 11.99,
+            },
+          },
+        },
+      },
+    };
+
+    // CALL DOM INVOKING FUNCTIONS HERE --------------------
+    savingsCalculator.onDomReady();
+    savingsCalculator.eventHandlers();
+    console.log(savingsCalculator.config);
+  },
+
+  onDomReady: () => {
+    console.log("Works");
+  },
+
+  // -------------------- HANDLE ALL PAGE LEVEL EVENTS --------------------
+  eventHandlers: () => {
+    // -----
+    const form = document.querySelector("form");
+    form.addEventListener("submit", handleSubmit);
+
+    // startButton.addEventListener("click", () => {
+    //   savingsCalculator.startGame();
+    // });
+  },
+};
+
+// -------------------- LOAD init() --------------------
+window.addEventListener("load", () => {
+  savingsCalculator.init();
+});
+
+
+
+
+
 const resultsDiv = document.getElementById("results"),
   fuelDiscount = 0.03,
   washDiscount = 0.10,
@@ -20,7 +118,3 @@ let mbGasPriceCad = mbGasPriceUs * exchangeRate,
 
 // ----- hideShowWash.js
 hideShowWashType(0)
-
-// -----
-const form = document.querySelector("form");
-form.addEventListener("submit", handleSubmit);
