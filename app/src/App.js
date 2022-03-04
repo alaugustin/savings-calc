82b1eaeea402c19bebffa774cfcca383a73eb04c
aaug <aaug@caasco.ca>
2022-03-04 11:13:33 -0500

    clean up

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// import Input from './Input';
-// import Label from './Label';
-
 function App() {
   const [submitting, setSubmitting] = useState(false);
   const handleSubmit = event => {
@@ -16,38 +13,6 @@ function App() {
     }, 3000)
   }
 
-  const [location, setLocation] = useState("on");
-  const locationChangeHandler = (e) => {
-    const { value } = e.target;
-    setLocation(value);
-  };
-
-  const [gas, setGas] = useState(0);
-  const gasChangeHandler = (e) => {
-    const { value } = e.target;
-    setGas(value);
-  };
-
-  const [wash, setWash] = useState(0);
-  const washChangeHandler = (e) => {
-    const { value } = e.target;
-    setWash(value);
-  };
-
-  const [washType, setWashType] = useState("-----");
-  //store number
-  const washTypeChangeHandler = (e) => {
-    const { value } = e.target;
-    setWashType(value);
-  };
-
-  const [sundries, setSundries] = useState(0);
-  //store number
-  const sundriesChangeHandler = (e) => {
-    const { value } = e.target;
-    setSundries(value);
-  };
-
   return (
     <div>
       <h1>Main Page</h1>
@@ -57,7 +22,7 @@ function App() {
       <form id="mainForm" onSubmit={handleSubmit}>
         <p>Select a province</p>
 
-        <div onChange={locationChangeHandler}>
+        <div>
           <label htmlFor="on"><input type="radio" id="on" value="on" name="location" value="on" />Ontario</label>
           <label htmlFor="mb"><input type="radio" id="mb" value="mb" name="location" value="mb" />Manitoba</label>
         </div>
@@ -65,21 +30,21 @@ function App() {
         <div>
           <label htmlFor="gas">
             How many tanks of gas per week? <br />
-            <input type="number" id="gas" name="location" value={gas} onChange={gasChangeHandler} />
+            <input type="number" id="gas" name="location" value="0" />
           </label>
         </div>
 
         <div>
           <label htmlFor="wash">
             How many car washes per week do you purchase along with gas? <br />
-            <input type="number" id="wash" name="location" value={wash} onChange={washChangeHandler} />
+            <input type="number" id="wash" name="location" value="0" />
           </label>
         </div>
 
         <div>
           <label htmlFor="wash">
             What type of car wash do you purchase? <br />
-            <select name="washType" id="washType" onChange={washTypeChangeHandler}>
+            <select name="washType" id="washType">
               <option value="">-----</option>
               <option value="basic">Basic</option>
               <option value="deluxe">Deluxe</option>
@@ -91,34 +56,9 @@ function App() {
         <div>
           <label htmlFor="wash">
             How much do you spend in-store per week? <br />
-            <input type="number" id="sundries" name="location" value={sundries} onChange={sundriesChangeHandler} />
+            <input type="number" id="sundries" name="location" value="0" />
           </label>
         </div>
-
-        {/* <Input labelType={"radio"} labelId={"on"} labelName={"location"} labelValue={state.locationOnValue} />
-        <Label htmlFor="on" labelCopy="Ontario" /><br />
-
-        <Input labelType={"radio"} labelId={"mb"} labelName={"location"} labelValue={state.locationMbValue} />
-        <Label htmlFor="mb" labelCopy="Manitoba" /><br />
-
-        <Label htmlFor="gas" labelCopy="How many tanks of gas per week?" /><br />
-        <Input labelType={"number"} labelId={"gas"} labelName={"gas"} labelValue={""} /><br />
-
-        <Label htmlFor="wash" labelCopy="How many car washes per week do you purchase along with gas?" /><br />
-        <Input labelType={"number"} labelId={"wash"} labelName={"wash"} labelValue={""} /><br />
-
-        <div id="washTypeHolder">
-          <Label htmlFor="washType" labelCopy="What type of car wash do you purchase?" /><br />
-          <select name="washType" id="washType">
-            <option value="">-----</option>
-            <option value="basic">Basic</option>
-            <option value="deluxe">Deluxe</option>
-            <option value="ultimate">Ultimate</option>
-          </select><br />
-        </div>
-
-        <Label htmlFor="sundries" labelCopy="How much do you spend in-store per week?" /><br />
-        <Input labelType={"number"} labelId={"sundries"} labelName={"sundries"} labelValue={""} /><br /> */}
 
         <button type="submit">Submit</button>
 
