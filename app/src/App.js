fb92da06fe708c1b1d77063dc396b4776492d888
aaug <aaug@caasco.ca>
2022-03-07 15:21:53 -0500

    adding AJAX API call

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useForm } from "react-hook-form";
+import React, { useState, useEffect } from 'react';
+// import { useForm } from "react-hook-form";
 import './App.css';
 
 function App() {
@@ -14,9 +14,51 @@ function App() {
     }, 3000)
   }
 
+  // ---------- API hook ----------
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.salestaxapi.ca/v2/province/ON")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+
+
+
   return (
     <div>
       <h1>Main Page</h1>
+
+      {error &&
+        <div>Error: {error.message}</div>
+      }
+
+       {!isLoaded &&
+        <div>Loading...</div>
+      }
+
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
 
       <hr />
       {/* https://react-hook-form.com/ */}
