220b50a6fc68cff04a6bfe9de38576446ababe06
aaug <aaug@caasco.ca>
2022-03-28 13:49:07 -0400

    formatting

--- app/public/js/resultsDisplay.js
+++ app/public/js/resultsDisplay.js
@@ -9,14 +9,14 @@ let fuelDiscountPerYear = (userSelectedNumberOfTanks, userSelectedGasPrice) => {
 
   gasResult.innerHTML = ``;
   gasResult.innerHTML = `
-    <p>Standard full tank of gas per week: ${cleanData.fullTankPerWeek}</p>
-    <p>User buys "${userSelectedNumberOfTanks}" tank(s) of gas a week</p>
-    <p>User selected gas price is: ${userSelectedGasPrice}</p>
+    <p>Standard full tank of gas per week: ${cleanData.fullTankPerWeek} litres</p>
+    <p>User buys ${userSelectedNumberOfTanks} tank(s) of gas a week</p>
+    <p>Average gas price for user is: $${userSelectedGasPrice}</p>
     <p>There are ${cleanData.weeksPerYear} weeks per year</p>
-    <p>The standard fuel discount is ${cleanData.fuelDiscount}</p>
-    <p>User spends "$${fuelCostPerYear.toFixed(2)}" per year</p>
-    <p>Fuel discount per year is ${fuelDiscountPerYear.toFixed(2)}</p>
-    <p>Fuel savings per year is ${fuelSavingsPerYear.toFixed(2)}</p>
+    <p>The standard fuel discount is $${cleanData.fuelDiscount} per litre</p>
+    <p>User buys $${fuelCostPerYear.toFixed(2)} in fuel per year</p>
+    <p>User saves a total of $${fuelDiscountPerYear.toFixed(2)}</p>
+    <p>Fuel savings $${fuelSavingsPerYear.toFixed(2)} per year after discount</p>
     <hr />
   `;
 }
@@ -29,12 +29,13 @@ let washDiscountPerYear = (userSelectedWashesPerWeek, userSelectedWashPrice, use
 
   washResult.innerHTML = ``;
   washResult.innerHTML = `
-    <p>User selected washes per week: ${userSelectedWashesPerWeek}</p>
+    <p>User purchases ${userSelectedWashesPerWeek} car washes per week</p>
     <p>User selected wash price is: ${userSelectedWashPrice}</p>
-    <p>User's regional tax rate is: ${userTaxRate}</p>
-    <p>One car wash with tax is: ${washWithTax.toFixed(2)}</p>
-    <p>User buys "$${washCostPerYear.toFixed(2)}" in washes a year</p>
-    <p>User saves a total of $${washDiscount.toFixed(2)} per year on washes a year</p>
+    <p>User's regional tax rate is: ${userTaxRate * 100}%</p>
+    <p>One week of car washes with tax is: ${washWithTax.toFixed(2)}</p>
+    <p>User buys $${washCostPerYear.toFixed(2)} in washes a year</p>
+    <p>User saves a total of $${(washCostPerYear * cleanData.purchaseDiscount).toFixed(2)}</p>
+    <p>After discount user spends a total of $${washDiscount.toFixed(2)} in car washes a year</p>
     <hr />
   `;
 }
@@ -46,12 +47,12 @@ let instoreDiscountPerYear = (userInstorePerWeek, userTaxRate) => {
 
   sundriesResult.innerHTML = ``;
   sundriesResult.innerHTML = `
-    <p>User spends per week: ${userInstorePerWeek}</p>
-    <p>User's regional tax rate is: ${userTaxRate}</p>
-    <p>User spends "$${instorePerWeekWitTax.toFixed(2)}" per week with tax</p>
-    <p>User buys "$${instoreCostPerYear.toFixed(2)}" in store a year</p>
-    <p>User saves a total of "$${(instoreCostPerYear * cleanData.purchaseDiscount).toFixed(2)}" in store a year</p>
-    <p>After discount user spends a total of "$${instoreDiscount.toFixed(2)}" in store a year</p>
+    <p>User spends $${userInstorePerWeek} in store per week</p>
+    <p>User's regional tax rate is: ${userTaxRate * 100}%</p>
+    <p>User spends $${instorePerWeekWitTax.toFixed(2)} per week with tax</p>
+    <p>User buys $${instoreCostPerYear.toFixed(2)} in store a year</p>
+    <p>User saves a total of $${(instoreCostPerYear * cleanData.purchaseDiscount).toFixed(2)}</p>
+    <p>After discount user spends a total of $${instoreDiscount.toFixed(2)} in store a year</p>
     <hr />
   `;
 };
